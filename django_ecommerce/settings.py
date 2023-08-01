14d60db6b732d6cab4913a70d816dbbd9d409c3a
Mr. Ely <my@email.me>
2023-08-01 14:28:12 +0330

    Add "django_render_partial" package & Break down base.html

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'django_render_partial',
 ]
 
 MIDDLEWARE = [
--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def header_component_view(request):
+    return render(request, 'header_component.html')
+
+
+def footer_component_view(request):
+    return render(request, 'footer_component.html')
