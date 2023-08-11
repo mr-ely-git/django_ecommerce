59110ca848ce6ae55f1e9bd78410e093b9646f6b
Mr. Ely <my@email.me>
2023-08-11 21:02:11 +0330

    Create contact us model

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ContactUsForm(models.Model):
+    email = models.EmailField()
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read_by_admin = models.BooleanField()
+
+
+
