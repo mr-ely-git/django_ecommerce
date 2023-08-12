729408886f032500dbf7311977bd3e8996ecd894
Mr. Ely <my@email.me>
2023-08-12 21:06:39 +0330

    Make Alert for a form field

--- contact_us/views.py
+++ contact_us/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 def contact_us_view(request):
     if request.method == 'POST':
+        if request.POST['email'] == '':
+            return render(request, 'contact_us_page.html', {'has_error': True})
         return redirect(reverse('index-page-url'))
     elif request.method == 'GET':
         pass
