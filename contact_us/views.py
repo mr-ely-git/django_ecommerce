e7f909c585df81c7967129e2ee9246809b5eb6d7
Mr. Ely <my@email.me>
2023-08-12 21:45:42 +0330

    Access to form data after submit

--- contact_us/views.py
+++ contact_us/views.py
@@ -10,5 +10,11 @@ def contact_us_view(request):
     #     return redirect(reverse('index-page-url'))
     # elif request.method == 'GET':
     #     pass
+    if request.method == 'POST':
+        contact_us_form = forms.ContactUsForm(request.POST)
+        if contact_us_form.is_valid():
+            print(contact_us_form.cleaned_data)
+            return redirect(reverse('index-page-url'))
+
     contact_us_form = forms.ContactUsForm()
     return render(request, 'contact_us_page.html', {'form': contact_us_form})
