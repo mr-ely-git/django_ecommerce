3292e323bea1746377dd11b885bc55c616fe8ed0
Mr. Ely <my@email.me>
2023-08-12 20:52:52 +0330

    Add contact us url to header menu & Add csrf_token to the contact us form & Redirect user after submit form

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def contact_us_view(request):
+    if request.method == 'POST':
+        return redirect(reverse('index-page-url'))
+    elif request.method == 'GET':
+        pass
     return render(request, 'contact_us_page.html')
