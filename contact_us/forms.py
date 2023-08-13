7747089cba819d6f05a0aa020fe8dafd7ff544a6
Mr. Ely <my@email.me>
2023-08-13 19:02:52 +0330

    Add classes and other attributes to the form elements

--- contact_us/forms.py
+++ contact_us/forms.py
@@ -2,6 +2,26 @@ from django import forms
 
 
 class ContactUsForm(forms.Form):
-    name = forms.CharField(label='Full Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    message = forms.CharField(label='Message', widget=forms.Textarea)
+    name = forms.CharField(label='نام و نام خانوادگی',
+                           max_length=100,
+                           widget=forms.TextInput(
+                               attrs={
+                                   'class': 'form-control',
+                                   'placeholder': 'نام خود را وارد کنید'
+                               },
+                           ))
+    email = forms.EmailField(label='ایمیل',
+                             widget=forms.TextInput(
+                                 attrs={
+                                     'class': 'form-control',
+                                     'placeholder': 'ایمیل خود را وارد کنید'
+                                 },
+                             ))
+    message = forms.CharField(label='متن پیام',
+                              widget=forms.Textarea(
+                                  attrs={
+                                      'id': 'message',
+                                      'class': 'form-control',
+                                      'placeholder': 'پیام خود را وارد کنید'
+                                  },
+                              ))
