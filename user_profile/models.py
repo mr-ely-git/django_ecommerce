ecdc0782a58a88346204152ee9040e668926ae76
Mr. Ely <my@email.me>
2023-08-23 03:02:35 +0330

    Add UserInformation model to user_profile app

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserInformation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=1000, null=True, blank=True)
+    mobile = models.CharField(max_length=20, null=True, blank=True, unique=True)
+    email_activation_code = models.CharField(max_length=150)
+    profile_image = models.ImageField(upload_to='profile_images/', null=True, blank=True)
+
